798bbe1c56b740241caf29f8555b292c7195d346
yangguojun <544759698@qq.com>
2020-07-07 13:33:21 +0800

    guava

--- src/main/java/com/yang/practice/guava/CollectionTest.java
+++ src/main/java/com/yang/practice/guava/CollectionTest.java
@@ -1,9 +1,12 @@
 package com.yang.practice.guava;
 
 import java.util.List;
+import java.util.Map;
 
 import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 /**
  * @Author: yangguojun01
@@ -14,6 +17,17 @@ public class CollectionTest {
     public static void main(String[] args) {
         List<String> list = Lists.newArrayList("aa", "bb", "cc");
         System.out.println(Joiner.on(",").join(list));
+        // Exception
+        //        list = null;
+        //        System.out.println(Joiner.on(",").join(list));
+        Map<String, Integer> map = Maps.newHashMap();
+        map.put("xiaoming", 12);
+        map.put("xiaohong", 13);
+        String result = Joiner.on(",").withKeyValueSeparator("=").join(map);
+        System.out.println(result);
+        String str = "1-2-3";
+        List<String> strlist = Splitter.on('-').splitToList(str);
+        System.out.println(strlist);
     }
 
 }
