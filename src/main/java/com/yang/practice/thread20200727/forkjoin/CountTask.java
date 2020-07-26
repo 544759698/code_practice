50a72e3503eb883ada0afb4a29625b6a4be753af
yangguojun <544759698@qq.com>
2020-07-27 02:20:25 +0800

    20200727

---
+++ src/main/java/com/yang/practice/thread20200727/forkjoin/CountTask.java
@@ -0,0 +1,68 @@
+package com.yang.practice.thread20200727.forkjoin;
+
+import java.util.ArrayList;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * @Author: yangguojun01
+ * @Date: 2020/7/27
+ */
+public class CountTask extends RecursiveTask<Long> {
+
+    private static final int THRESHOLD = 10000;
+    private long start;
+    private long end;
+
+    public CountTask(long start, long end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    /**
+     * The main computation performed by this task.
+     *
+     * @return the result of the computation
+     */
+    @Override
+    protected Long compute() {
+        long sum = 0;
+        boolean canCompute = (end - start) < THRESHOLD;
+        if (canCompute) {
+            for (long i = start; i <= end; i++) {
+                sum += i;
+            }
+        } else {
+            long step = (end - start) / 100;
+            ArrayList<CountTask> subTasks = new ArrayList<>();
+            long pos = start;
+            for (int i = 0; i < 100; i++) {
+                long lastOne = pos + step;
+                if (lastOne > end) {
+                    lastOne = end;
+                }
+                CountTask subTask = new CountTask(pos, lastOne);
+                pos += step + 1;
+                subTasks.add(subTask);
+                subTask.fork();
+            }
+            for (CountTask t : subTasks) {
+                sum += t.join();
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        CountTask task = new CountTask(0, 20000L);
+        ForkJoinTask<Long> result = forkJoinPool.submit(task);
+        try {
+            Long sum = result.get();
+            System.out.println(sum);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
