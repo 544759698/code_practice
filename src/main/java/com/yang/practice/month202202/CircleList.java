251fdf95d2357ba0c77bb012948870743078230f
yangguojun <544759698@qq.com>
2022-02-03 18:18:58 +0800

    day20220203

---
+++ src/main/java/com/yang/practice/month202202/CircleList.java
@@ -0,0 +1,23 @@
+package com.yang.practice.month202202;
+
+/**
+ * @Author: yangguojun01
+ * @Date: 2022/2/3
+ */
+public class CircleList {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
