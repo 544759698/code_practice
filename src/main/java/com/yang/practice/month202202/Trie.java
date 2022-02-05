8aa091fb192a1e8dea079a2a602af5d151b30cc1
yangguojun <544759698@qq.com>
2022-02-05 23:57:57 +0800

    day20220205

---
+++ src/main/java/com/yang/practice/month202202/Trie.java
@@ -0,0 +1,58 @@
+package com.yang.practice.month202202;
+
+/**
+ * @Author: yangguojun01
+ * @Date: 2022/2/5
+ */
+public class Trie {
+
+    Trie[] children;
+    boolean isEnd;
+
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
+    }
+
+    public void insert(String word) {
+        if (word == null || word == "") {
+            return;
+        }
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            if (node.children[word.charAt(i) - 'a'] == null) {
+                node.children[word.charAt(i) - 'a'] = new Trie();
+            }
+            node = node.children[word.charAt(i) - 'a'];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        if (word == null || word == "") {
+            return true;
+        }
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            if (node.children[word.charAt(i) - 'a'] == null) {
+                return false;
+            }
+            node = node.children[word.charAt(i) - 'a'];
+        }
+        return node.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        if (prefix == null || prefix == "") {
+            return true;
+        }
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            if (node.children[prefix.charAt(i) - 'a'] == null) {
+                return false;
+            }
+            node = node.children[prefix.charAt(i) - 'a'];
+        }
+        return node != null;
+    }
+}
