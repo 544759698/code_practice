38b0f3d4547b3480fdd054c5cc1272fb79b150fb
yangguojun <544759698@qq.com>
2022-03-03 23:40:21 +0800

    day20220303

---
+++ src/main/java/com/yang/practice/month202203/L06CircleList.java
@@ -0,0 +1,33 @@
+package com.yang.practice.month202203;
+
+/**
+ * @Author: yangguojun01
+ * @Date: 2022/3/3
+ */
+public class L06CircleList {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
