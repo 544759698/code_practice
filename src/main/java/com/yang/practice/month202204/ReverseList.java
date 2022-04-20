0b6bfc08d5b50ed88ccc4459faeb6e26507aaa6a
yangguojun <544759698@qq.com>
2022-04-20 22:37:10 +0800

    day20220420

--- src/main/java/com/yang/practice/month202204/ReverseList.java
+++ src/main/java/com/yang/practice/month202204/ReverseList.java
@@ -21,8 +21,15 @@ class ListNode {
     int val;
     ListNode next;
 
-    ListNode(int x) {
-        val = x;
-        next = null;
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
