3058e235f5994b98170aceff0d33fc9361e1ed31
yangguojun <544759698@qq.com>
2022-05-09 01:05:44 +0800

    Create MaxSubArr.java

---
+++ src/main/java/com/yang/practice/month202205/MaxSubArr.java
@@ -0,0 +1,18 @@
+package com.yang.practice.month202205;
+
+/**
+ * @Author: yangguojun01
+ * @Date: 2022/5/9
+ */
+public class MaxSubArr {
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = dp[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
