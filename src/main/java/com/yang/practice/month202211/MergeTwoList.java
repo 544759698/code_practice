ae9215b1ba7ce82468e5cecbce389e1a4760c18f
yangguojun <544759698@qq.com>
2022-11-03 00:17:06 +0800

    Create MergeTwoList.java

---
+++ src/main/java/com/yang/practice/month202211/MergeTwoList.java
@@ -0,0 +1,24 @@
+package com.yang.practice.month202211;
+
+/**
+ * @Author: yangguojun01
+ * @Date: 2022/11/2
+ */
+public class MergeTwoList {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode pre = new ListNode(-1);
+        ListNode dump = pre;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                pre.next = list1;
+                list1 = list1.next;
+            } else {
+                pre.next = list2;
+                list2 = list2.next;
+            }
+            pre = pre.next;
+        }
+        pre.next = list1 != null ? list1 : list2;
+        return dump.next;
+    }
+}
