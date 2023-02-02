135c066a7dbdf31e3ed4957ad3518af3679eaf68
yangguojun <544759698@qq.com>
2023-02-02 23:46:59 +0800

    Create BinarySearch.java

---
+++ src/main/java/com/yang/practice/month202302/BinarySearch.java
@@ -0,0 +1,23 @@
+package com.yang.practice.month202302;
+
+/**
+ * @Author: yangguojun01
+ * @Date: 2023/2/2
+ */
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
