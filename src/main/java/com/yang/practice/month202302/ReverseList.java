bea1ac60b078cfcee23588e9966551e6b2cdcbec
yangguojun <544759698@qq.com>
2023-02-05 20:55:07 +0800

    Create ReverseList.java

---
+++ src/main/java/com/yang/practice/month202302/ReverseList.java
@@ -0,0 +1,36 @@
+package com.yang.practice.month202302;
+
+/**
+ * @Author: yangguojun01
+ * @Date: 2023/2/5
+ */
+public class ReverseList {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode tmp = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = tmp;
+        }
+        return pre;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
