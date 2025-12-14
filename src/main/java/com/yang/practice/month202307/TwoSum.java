860fa482b28c06c3da4077e2e3d887cab996dbe5
yangguojun <544759698@qq.com>
2025-12-14 20:42:28 +0800

    Update TwoSum.java

--- src/main/java/com/yang/practice/month202307/TwoSum.java
+++ src/main/java/com/yang/practice/month202307/TwoSum.java
@@ -21,8 +21,19 @@ public class TwoSum {
 
     public static void main(String[] args) {
         TwoSum ts = new TwoSum();
-        System.out.println(ts.findTarget(new int[] {2, 4, 5, 7, 10}, 12));
-        System.out.println(ts.findTarget(new int[] {2, 4, 5, 7, 10}, 9));
-        System.out.println(ts.findTarget(new int[] {2, 4, 5, 7, 10}, 8));
+        System.out.println(ts.findTarget(new int[]{2, 4, 5, 7, 10}, 12));
+        System.out.println(ts.findTarget(new int[]{2, 4, 5, 7, 10}, 9));
+        System.out.println(ts.findTarget(new int[]{2, 4, 5, 7, 10}, 8));
+    }
+
+    public boolean findTarget1(int[] nums, int target) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.add(target - nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
     }
 }
